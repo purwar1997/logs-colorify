427f3fe85dbaa46bcdff4d5742ef89321ef1fe3a
purwar1997 <shubhampurwar511@gmail.com>
2025-04-30 21:55:45 +0530

    feat: add more properties to format logs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 export class Log {
   public static error(message: string) {
-    console.log(`%c ${message}`, 'color: red');
+    console.log(`%c ${message}`, 'color: red; font-size: 14px');
   }
 
   public static warn(message: string) {
-    console.log(`%c ${message}`, 'color: yellow');
+    console.log(`%c ${message}`, 'color: yellow; font-size: 14px');
   }
 
   public static info(message: string) {
-    console.log(`%c ${message}`, 'color: black; background-color: yellow');
+    console.log(`%c ${message}`, 'color: black; background-color: yellow; font-size: 14px');
   }
 
   public static debug(message: string) {
-    console.log(`%c ${message}`, 'color: blue');
+    console.log(`%c ${message}`, 'color: green; font-size: 14px');
   }
 }
